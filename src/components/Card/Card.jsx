f2af8a3af46d5474283ded15e5e5a7d8c63f1b84
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2026-01-17 11:21:48 -0300

    refact: add delete button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,13 @@
 import "./Card.css"
-export function Card({price, image, title}){
+export function Card({id, price, image, title, onDelete}){
     return(
         <div className="card">
             <img src={image} alt="" />
             <h2>{title}</h2>
             <p>Preço: {price}</p>
+            <button className="btn_delete" onClick={() => onDelete(id)}>
+                Excluir
+            </button>
         </div>
     )
 }
